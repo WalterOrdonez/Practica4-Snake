81c56cb91c6527bbb5307999d34149dea165dd95
WalterOrdonez <walter.ordonez.garcia@gmail.com>
2017-04-29 21:40:23 -0600

    Metodos de Usuario

--- Practica_Snake/Usuario.cs
+++ Practica_Snake/Usuario.cs
@@ -8,6 +8,32 @@ namespace Practica_Snake
 {
     class Usuario
     {
+        String nombre;
+        String pass;
+        public void setNombre(String nom)
+        {
+            this.nombre = nom;
+        }
+        public void setPass(String pas)
+        {
+            this.pass = pas;
+        }
 
+        public String getNombre()
+        {
+            return this.nombre;
+        }
+        public String getPass()
+        {
+            return this.pass;
+        }
+        public bool compararNombres(String nom)
+        {
+            return (this.nombre.Equals(nom, StringComparison.OrdinalIgnoreCase));
+        }
+        public bool compararPass(String pass)
+        {
+            return (this.pass.Equals(pass, StringComparison.OrdinalIgnoreCase));
+        }
     }
 }
