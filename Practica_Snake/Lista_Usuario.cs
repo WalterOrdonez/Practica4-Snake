77cf910282e313f987b6c98653f864321107c639
WalterOrdonez <walter.ordonez.garcia@gmail.com>
2017-04-29 21:43:03 -0600

    Creacion lista usuario

---
+++ Practica_Snake/Lista_Usuario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Snake
+{
+    class Lista_Usuario 
+    {
+        ArrayList Usuarios;
+        public Lista_Usuario()
+        {
+            Usuarios = new ArrayList();
+        }
+        public void addUsuario(String Nombre, String Pass){
+            Usuario nuevo=new Usuario();
+            nuevo.setNombre(Nombre);
+            nuevo.setPass(Pass);
+            Usuarios.Add(nuevo);
+        }
+        public void deleteUsuario(String Nombre)
+        {
+            int posicion=buscarUsuario(Nombre);
+            if (posicion>=0)
+            {
+                Usuarios.RemoveAt(posicion);
+            }
+        }
+        public int buscarUsuario(String Nombre)
+        {
+            foreach (Usuario u in Usuarios)
+            {
+                if (u.compararNombres(Nombre))
+                {
+                    return Usuarios.IndexOf(u);
+                }
+            }
+            return -1;
+        }
+    }
+}
