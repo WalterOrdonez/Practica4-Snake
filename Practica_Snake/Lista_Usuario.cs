62a4f4691f02517a3ef11ccc523b856fc33033c5
WalterOrdonez <walter.ordonez.garcia@gmail.com>
2017-04-30 19:02:03 -0600

    Generar Reportes

--- Practica_Snake/Lista_Usuario.cs
+++ Practica_Snake/Lista_Usuario.cs
@@ -10,10 +10,15 @@ namespace Practica_Snake
     class Lista_Usuario 
     {
         ArrayList Usuarios;
+        ArrayList Puntuaciones;
+        ArrayList Tiempos;
         Usuario Administrador;
         public Lista_Usuario()
         {
             Usuarios = new ArrayList();
+            Puntuaciones = new ArrayList();
+            Tiempos = new ArrayList();
+            Administrador = new Usuario();
             Administrador.setNombre("Admin_ORGA");
             Administrador.setPass("12345");
         }
@@ -22,6 +27,8 @@ namespace Practica_Snake
             nuevo.setNombre(Nombre);
             nuevo.setPass(Pass);
             Usuarios.Add(nuevo);
+            Puntuaciones.Add(nuevo);
+            Tiempos.Add(nuevo);
         }
         public void deleteUsuario(String Nombre)
         {
@@ -42,6 +49,40 @@ namespace Practica_Snake
             }
             return -1;
         }
+        private void ordenarTiempo()
+        {
+            Tiempos.Sort(new ComparaTiempos());
+        }
+        private void ordenarPuntos()
+        {
+            Puntuaciones.Sort(new ComparaPuntos());
+        }
+        public ArrayList generarReporteTiempo()
+        {
+            ordenarTiempo();
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"Tiempo.rep"))
+            {
+                foreach (Usuario U in Tiempos)
+                {
+                    file.WriteLine("Usuario: "+U.getNombre()+" Nivel: "+U.getNivel()+" Tiempo: "+U.getTiempo()+" seg");
+                    file.WriteLine();
+                }
+            }
+            return Tiempos;
+        }
+        public ArrayList generarReportePuntos()
+        {
+            ordenarPuntos();
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"Puntos.rep"))
+            {
+                foreach (Usuario U in Puntuaciones)
+                {
+                    file.WriteLine("Usuario: "+U.getNombre() + " Nivel: " + U.getNivel() + " Puntos: " + U.getPuntos());
+                    file.WriteLine();
+                }
+            }
+            return Puntuaciones;
+        }
         public void modificarMarcas(String Nombre, int Nivel, int Puntos, int Tiempo)
         {
             int posicion = buscarUsuario(Nombre);
@@ -57,6 +98,20 @@ namespace Practica_Snake
             {
                 Console.WriteLine("Usuarios({0}) is out of range.", posicion);
             }
+        }
+    }
+    public class ComparaTiempos : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return ((Usuario)y).getTiempo() - ((Usuario)x).getTiempo();
+        }
+    }
+    public class ComparaPuntos : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return ((Usuario)y).getPuntos() - ((Usuario)x).getPuntos();
         }
     }
 }
