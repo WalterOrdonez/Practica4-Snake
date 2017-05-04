123e0071c6297378d98e85af227aaf39b34e155d
WalterOrdonez <walter.ordonez.garcia@gmail.com>
2017-05-03 18:00:30 -0600

    Se agregaron las clases para el manejo de la serpiente, Snake

---
+++ Practica_Snake/nodoSnake.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Snake
+{
+    class nodoSnake
+    {
+        int x;
+        int y;
+        nodoSnake siguiente;
+        public nodoSnake()
+        {
+            x = 0;
+            y = 0;
+            siguiente = null;
+        }
+        public void setX(int x)
+        {
+            this.x = x;
+        }
+        public void setY(int y)
+        {
+            this.y = y;
+        }
+        public void setSiguiente(nodoSnake siguiente)
+        {
+            this.siguiente = siguiente;
+        }
+        public int getX()
+        {
+            return this.x;
+        }
+        public int getY()
+        {
+            return this.y;
+        }
+        public nodoSnake getSiguiente()
+        {
+            return this.siguiente;
+        }
+    }
+}
---
+++ Practica_Snake/snake.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Snake
+{
+    class Snake
+    {
+        const int derecha = 1;
+        const int arriba = 2;
+        const int izquierda = 3;
+        const int abajo = 4;
+        
+        nodoSnake Cabeza;
+        nodoSnake Ultimo;
+        int direccion;
+        
+        public Snake()
+        {
+            Cabeza = new nodoSnake();
+            Cabeza.setX(4);
+            Cabeza.setY(2);
+            direccion = 1;
+        }
+        public void Crecer()
+        {
+
+        }
+
+    }
+}
