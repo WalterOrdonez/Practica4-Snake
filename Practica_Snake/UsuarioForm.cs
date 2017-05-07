f24a9ab7a11df2cf05244737d3f62f91f7623cdc
MacoChave <totto_cha@hotmail.com>
2017-05-07 11:21:53 -0600

    Bugs corregidos

--- Practica_Snake/UsuarioForm.cs
+++ Practica_Snake/UsuarioForm.cs
@@ -6,7 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Threading; 
+using System.Threading;
+using System; 
 
 namespace Practica_Snake
 {
